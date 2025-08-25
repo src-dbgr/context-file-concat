6801bbf223c4c435ed0588d8994229a5b31d75ee
src-dbgr <src-dbgr@gmail.com>
2025-08-25 23:14:53 +0200

    fix re-scan button logo

--- src/ui/src/lib/components/Sidebar.svelte
+++ src/ui/src/lib/components/Sidebar.svelte
@@ -192,12 +192,14 @@
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
           stroke-width="2"
         >
-          <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
-          <path d="M21 3v5h-5" />
-          <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1 -6.74 -2.74L3 16" />
-          <path d="M3 21v-5h5" />
+          <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"></path>
+          <path d="M21 3v5h-5"></path>
+          <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"></path>
+          <path d="M8 16H3v5"></path>
         </svg>
       {:else}
         <svg
@@ -205,12 +207,14 @@
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
           stroke-width="2"
         >
-          <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
-          <path d="M21 3v5h-5" />
-          <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1 -6.74 -2.74L3 16" />
-          <path d="M3 21v-5h-5" />
+          <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"></path>
+          <path d="M21 3v5h-5"></path>
+          <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"></path>
+          <path d="M8 16H3v5"></path>
         </svg>
       {/if}
       {$appState.is_scanning ? "Scanning..." : "Re-Scan"}
