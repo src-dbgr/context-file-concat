83fc96e4953bfb343c8e75669dad6a2173008a52
src-dbgr <src-dbgr@gmail.com>
2025-09-07 23:17:16 +0200

    remove obsolte select dir button

--- src/ui/src/lib/components/Header.svelte
+++ src/ui/src/lib/components/Header.svelte
@@ -14,26 +14,6 @@
 <div class="top-bar">
   <div class="path-selection">
     <button
-      id="select-dir-btn"
-      onclick={() => post("selectDirectory")}
-      disabled={is_scanning}
-    >
-      {#if is_scanning}
-        <svg class="icon" viewBox="0 0 24 24">
-          <circle cx="12" cy="12" r="10" />
-          <polyline points="12,6 12,12 16,14" />
-        </svg>
-        {$t("sidebar.scanning")}
-      {:else}
-        <svg class="icon" viewBox="0 0 24 24">
-          <path
-            d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z"
-          />
-        </svg>
-        {$t("action.selectDirectory")}
-      {/if}
-    </button>
-    <button
       id="clear-dir-btn"
       title={$t("action.clear")}
       style:display={current_path ? "inline-flex" : "none"}
